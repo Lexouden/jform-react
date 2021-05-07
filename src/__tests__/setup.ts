bb916f19ac68cc15115bae4c968054e3b9c7c981
alex <alex@cak.nl>
2021-05-07 14:48:39 +0200

    Fixed react error in jest

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,10 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+import React from 'react';
+React.useLayoutEffect = React.useEffect;
+
 // @ts-ignore
 Enzyme.configure({ adapter: new Adapter() });
 
 // Fail tests on any warning
-console.error = (message) => {
-  throw new Error(message);
-};
